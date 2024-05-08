cc420285909060b657386880219d7cb802f1d167
Stefano Angioletti-Uberti <67963282+sangioletti@users.noreply.github.com>
2024-05-08 12:20:07 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # fluorescence_fit
-Repository with the code to fit fluorescence data for Ig-mediated adsorption
+Repository with the code to fit fluorescence data for Ig-mediated adsorption.
+
+Needs the following libraries to be installed (they usually come with a standard anaconda installation):
+
+- numpy 
+- scipy
+- python (version 3.8 or higher works, lower versions not tested)
---
+++ final_theory.py
@@ -0,0 +1,194 @@
+import numpy as np
+import pandas as pd
+from scipy.optimize import basinhopping
+import matplotlib.pyplot as plt
+import scipy
+
+# Define the function to fit
+def sample_function( x, a, b, c, d, e):
+    '''This is the fitting function used to fit the fluorescence
+    intensity signal as a function of "x", the number of IgM present on a cell
+    surface'''
+    if ( 1.0 + c * x ).any() <= 0 and d % 1 != 0:  
+        print(f"Warning: Trying to compute a complex number with x={x} and exponent={b}")
+        return np.nan  # Return NaN or some other appropriate value
+    p1 = ( 1.0 + c * x )**d
+    return a + b * e * p1 / ( 1.0 + e * p1 )
+
+def sample_log( x, a, b, c, d, e):
+    '''Just returns the logarithm of the sampling function'''
+    return np.log( sample_function( x, a, b, c, d, e ) )
+
+def logarithmic_derivative( x, a, b, c, d, e ):
+    '''Logarithmic derivative of the sampling function, defined as
+    dln(f)/dln(x)
+    '''
+    der_sample = b * c * d * e * ( 1.0 + c * x )**(d-1.0) / (1.0 + e * (1.0 + c* x)**d )**2
+    y = sample_function( x,a,b,c,d,e) 
+    return x/y * der_sample
+
+def middle( x, a, b, c, d, e ):
+    '''Average between the maximum and minimum value of the logarithm  
+    of the sampling function'''
+    my_min = a + b * e / ( 1.0 + e )
+    my_max = a + b
+    log_min = np.log( my_min ) 
+    log_max = np.log( my_max ) 
+    log_ave = ( log_min + log_max ) / 2.0
+    return log_ave
+
+def find_middle( x, a, b, c, d, e ):
+    '''Function used to solve the equation log(f) - x0 = 0, where f is the sampling function
+    and x0 the mid point defined by "middle"'''
+    return np.log( sample_function( x, a, b, c, d, e ) ) - middle( x, a, b, c, d, e )
+
+def find_onset( x, y_value_min, b, alpha ):
+    return np.log( b * x**alpha ) - np.log( y_value_min )
+
+    
+def geometric_onset( x_data, y_data, opt_params, verbose = False ):
+    # Find mid point between max and minimum in logarithmic space and calculate the logarithmic derivative 
+    # at that point.
+    # Starting guess for mid point
+    #x0 = np.max( x_data ) - np.min( x_data )
+    root = scipy.optimize.bisect(find_middle, np.min( x_data), np.max( x_data ), args = tuple( opt_params ) )
+
+    # Next line needed to format in a way I can feed this to function, probably there is a smarter way
+    alpha = logarithmic_derivative( root, *opt_params )
+
+    aa = opt_params[ 0 ]
+    bb = opt_params[ 1 ]
+    cc = opt_params[ 2 ]
+    dd = opt_params[ 3 ]
+    ee = opt_params[ 4 ]
+
+    y_value_min = aa + bb * ee / ( 1.0 + ee )
+    y_value_max = aa + bb 
+
+    #Define the curve which in a log-log plot looks like a line with the right slope
+    log_mid_point = ( np.log( y_value_min ) + np.log( y_value_max ) ) / 2.0 
+    y0 = np.exp(1)**( log_mid_point )  # y value of the mid point
+    b_coeff = y0 / (root**alpha)
+    y_derivative = b_coeff * x_data**alpha 
+    args_find_onset = ( y_value_min, b_coeff, alpha )
+
+    #Find onset using bisection method
+    onset = scipy.optimize.bisect( find_onset, 0, np.max( x_data), args = args_find_onset )
+    if verbose:
+      print( f'x value corresponding to the mid point between max and min fluorescence on log-log plot = {root}' )
+      print( f'y value for the midpoint is {y0}' )
+      print( f'Derivative at midpoint is: {alpha}' )
+      print( f"Onset found at {onset}" )
+
+    return onset, b_coeff, alpha, y0, y_value_min
+    
+
+def calculate_onset( my_file, sheet_name, x_name, y_name, bounds, initial_guess, output = 'output.txt', graph_name = 'LogLog.pdf', verbose = False,
+                     same_scale = False, mc_runs = 8, n_hopping = 1000, T_hopping = 1 ):
+    data = pd.read_excel( my_file, sheet_name = sheet_name )
+    x_data = data[x_name].values  
+    y_data = data[y_name].values
+
+    #Clean the data to remove values with negative intensity. Alternatively, one can simply shift all values so 
+    #that the minimum intensity is positive, necessary for taking the logarithm later
+    condition = y_data > 0.0
+    x_data = np.extract( condition, x_data ) 
+    y_data = np.extract( condition, y_data ) 
+   
+    # Not good python practice...but here we define objective function within another function 
+    def objective_function( params ):
+        '''Define the objective function to minimize (average residuals between data and model) via the 
+        basin hopping Monte Carlo procedure'''
+        return np.sqrt( np.sum((np.log( y_data ) - sample_log( x_data, *params))**2) / len( y_data ) )
+
+    num_param = 5 # This is the number of parameters in our fitting function
+    all_opt_params = np.zeros( ( mc_runs, num_param + 5 ) )
+    weights = np.ones( mc_runs )
+    best = np.inf
+    best_sample = -1
+    for i in range( mc_runs ):
+      # Perform basin hopping optimization
+      result = basinhopping( objective_function, initial_guess, niter = n_hopping, T = T_hopping, minimizer_kwargs={"bounds" : bounds})
+      if result.fun < best:
+        best_sample = i
+        best = result.fun
+      weights[ i ] = 1.0 / result.fun
+      all_opt_params[ i, :-5 ] = result.x
+      onset, b_coeff, alpha, y0, y_min_value = geometric_onset( x_data, y_data, all_opt_params[ i, :-5 ], verbose = verbose )
+      all_opt_params[ i, -1 ] = onset
+      all_opt_params[ i, -2 ] = b_coeff 
+      all_opt_params[ i, -3 ] = alpha
+      all_opt_params[ i, -4 ] = y0
+      all_opt_params[ i, -5 ] = y_min_value
+
+    for i in range( mc_runs ):
+      print( f"{i}, {all_opt_params[ i, :-5 ]}" )
+
+    opt_params = np.average( all_opt_params, axis = 0, weights = weights ) 
+    error_onset = np.sqrt( np.var( all_opt_params[ -1 ], axis = 0 ) ) 
+    average_onset = opt_params[ -1 ]
+    onset = all_opt_params[ best_sample, -1 ]
+    b_coeff = all_opt_params[ best_sample, -2 ]
+    alpha = all_opt_params[ best_sample, -3 ]
+    y0 = all_opt_params[ best_sample, -4 ]
+    y_value_min = all_opt_params[ best_sample, -5 ]
+    
+    # Print the optimized parameters
+    if verbose:
+      print("Parameter for best solution:", all_opt_params[ best_sample ] )
+    with open( output, 'w+' ) as my_f:
+      par = [ "A", "B", "C", "D", "E" ]
+      my_f.write( "Intensity approximated with fitting function A + B * E * ( 1.0 + C * x )^D / [ 1.0 + E * ( 1.0 + C * x )^D ] \n" )
+      for name, val in zip( par, all_opt_params[ best_sample ] ):
+        my_f.write( f"{name}  {val} \n" ) 
+      #print("Optimized Parameters:", result.x)
+      my_f.write( f'Logarithmic Derivative at midpoint (superselectivity parameter): {alpha} \n' )
+      my_f.write( f'Onset for best solution at x = {onset} \n' )
+      my_f.write( f'AVERAGE onset found at x = {average_onset} \n' )
+      my_f.write( f'ERROR on average onset: {error_onset} \n' )
+
+    # Ok, now prepare to make the plots
+    x = np.logspace( np.log10( np.min( x_data )), np.log10( np.max( x_data ) ), 100 )
+    y_min = np.ones( 100 ) * y_value_min
+    y_mid = np.ones( 100 ) * y0
+    y_onset = np.logspace( -1,5, 100 )
+    x_onset = np.ones( 100 ) * onset
+    x_onset_plus = np.ones( 100 ) * ( onset + error_onset )
+    if onset - error_onset > 0:
+      if verbose:
+        print( "error_onset smaller then onset" ) 
+      x_onset_minus = np.ones( 100 ) * ( onset - error_onset )
+    else: 
+      x_onset_minus = np.ones( 100 ) * np.min( x_data )
+
+    y_derivative = b_coeff * x_data**alpha 
+    
+    # Plot the best fit function 
+    #y_fit = sample_function( x, a = opt_params[ 0 ], b = opt_params[ 1 ], c = opt_params[ 2 ], d = opt_params[ 3 ], e = opt_params[ 4 ] ) 
+    y_fit = sample_function( x, 
+                             a = all_opt_params[ best_sample, 0 ], 
+                             b = all_opt_params[ best_sample, 1 ], 
+                             c = all_opt_params[ best_sample, 2 ], 
+                             d = all_opt_params[ best_sample, 3 ], 
+                             e = all_opt_params[ best_sample, 4 ] ) 
+
+    plt.xscale( 'log' )
+    plt.yscale( 'log' )
+    if same_scale:
+      yrange = np.max( y_data ) / np.min( y_data )
+      plt.xlim( np.min( x_data ), yrange * np.min( x_data ) )
+    else:
+      plt.xlim( np.min( x_data ), np.max( x_data ))
+
+    plt.ylim( np.min( y_data ), np.max( y_data ) )
+    plt.plot( x_data, y_data, 'g.', ms = 1.0, label = 'Exp.' )  # Experimental data
+    plt.plot( x, y_fit, 'b-', ms = 0.6, label = 'Best fit' )   # Fitted curve
+    plt.plot( x_data, y_derivative, 'r-.', ms = 0.3, label = 'Tangent' ) # Plot of the tangent in the log-log plot
+    plt.plot( x, y_mid, 'k-', ms = 0.3, label = 'Mid-point' )   # Horizontal line at the mid point in the log-log plot 
+    plt.plot( x_onset, y_onset, 'k--', ms = 0.3, label = "Onset" ) # A vertical line at the onset
+    plt.plot( x, y_min, 'k-', ms = 0.3, label = 'Minimum' )     # Horizontal line at the minimum (extrapolated at x = 0 )
+    #plt.plot( x_onset_plus, y_onset, 'r-.', ms = 0.3, label = "Onset + $\Delta$" ) # A vertical line at the onset
+    #plt.plot( x_onset_minus, y_onset, 'r-.', ms = 0.3, label = "Onset - $\Delta$" ) # A vertical line at the onset
+    plt.legend()
+    plt.savefig( graph_name )
+    plt.close()
