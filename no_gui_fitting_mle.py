ef9ad43655ab5908bfd706d0c8cdf76209afccbe
sangioletti <67963282+sangioletti@users.noreply.github.com>
2024-06-14 03:09:54 +0100

    gp too slow

--- no_gui_fitting_mle.py
+++ no_gui_fitting_mle.py
@@ -2,20 +2,18 @@ import sys
 import numpy as np
 from sklearn.gaussian_process.kernels import RBF
 
-code_directory = "/Users/sangiole/Dropbox/Papers_data_live/Australia-immunology/fit_curves/fluorescence_fit" 
+code_directory = "/Users/sangiole/Github-repos/fluorescence_fit/"
 
 print( f"ATTENTION: Looking for python scripts in {code_directory}" )
 
 try:
-  from theory import sample_function, sample_constant, find_best_fit, calculate_onset_stat
-  from theory import plot_fitted_curve, extract_and_clean_data, model_variance 
-  from theory import no_signal, compare_models_using_p_value
+  from theory import plot_fitted_curve, extract_and_clean_data, find_best_fit
+  from theory import find_best_fit_mle, AIC, no_signal, sample_function
 except:
   # Change the line belows to import python package from the correct directory if needed
   sys.path.append( code_directory )
-  from theory import sample_function, sample_constant, find_best_fit, calculate_onset_stat 
-  from theory import plot_fitted_curve, extract_and_clean_data, model_variance 
-  from theory import no_signal, compare_models_using_p_value
+  from theory import plot_fitted_curve, extract_and_clean_data, find_best_fit
+  from theory import find_best_fit_mle, AIC, no_signal, sample_function
 
 # Change the line belows to import data from the correct excel files and their internal pages
 my_file = "Expt 10 IgM+ only IgG-dextran.xlsx"
@@ -44,14 +42,13 @@ print( f"""This code takes the data from '{my_file}', specifically, from the she
 
 # PARAMETERS USED. DO NOT CHANGE UNLESS YOU KNOW WHAT YOU ARE DOING.
 
-# Parameters for fitting using MC basin hopping
-mc_runs = 8 
-n_hopping = 2000
-T_hopping = 3
+# Parameters for fitting 
 logarithmic_fit = True
-
-# p value to compare multivalent binding model to model where no signal is present 
-p_for_significance = 0.05
+kernel = RBF
+#alpha_gp = 1e-8, # Small alpha for likelihood
+alpha_gp = 1e-2, # Small alpha for likelihood
+n_restarts_optimizer_gp = 5 
+#n_restarts_optimizer_gp = 10 
 
 # bounds (lower, upper) for the parameters inside the fitting function
 bound_A = ( 0, 1000 )       # Defines the lower bound for the measured intensity
@@ -86,49 +83,79 @@ for name in sheet_names:
 
   x_data_all, y_data_all = extract_and_clean_data( my_file, name, x_name, y_name )
 
+  #x_data_all = x_data_all[::5]
+  #y_data_all = y_data_all[::5]
+
   #Ok, so let's first fit the data so we obtain the variance around the best fit
 
   print( "Try fitting model where signal is assumed to exist" )
   #First try to fit a model where there should be a signal present
+  par, _, best_sample, _ = find_best_fit(  
+                                                     x_data = x_data_all, 
+                                                     y_data = y_data_all, 
+                                                     bounds = bounds, 
+                                                     initial_guess = initial_guess, 
+                                                     function_type = "multivalent",
+                                                     onset_fitting = False,
+                                                     mc_runs = 1, 
+                                                     n_hopping = 1000, 
+                                                     #n_hopping = n_hopping, 
+                                                     #T_hopping = T_hopping 
+                                                     )
 
-  optimal_parameters, onset_parameters = find_best_fit_mle( x_data, y_data, 
+  initial_guess = par[ best_sample, :5]
+  print( f"Initial guess for gp optimisation {initial_guess}" )
+
+  optimal_parameters_multi, log_likelyhood_multi, onset_parameters = find_best_fit_mle(
+                   sample_function, 
+                   x_data_all, y_data_all, 
                    bounds, initial_guess, function_type = "multivalent",
-                   kernel = RBF(),
-                   alpha_gp =1e-10, # Small alpha for likelihood
-                   n_restarts_optimizer_gp = 10 
+                   kernel = kernel,
+                   alpha_gp = alpha_gp, # Small alpha for likelihood
+                   n_restarts_optimizer_gp = n_restarts_optimizer_gp, 
                    onset_fitting = True,
                    verbose = False )
   
-  best_param_multivalent = {} 
-  best_param_multivalent[ "a" ] = optimal_parameters[ 0 ]
-  best_param_multivalent[ "b" ] = optimal_parameters[ 1 ]
-  best_param_multivalent[ "c" ] = optimal_parameters[ 2 ]
-  best_param_multivalent[ "d" ] = optimal_parameters[ 3 ]
-  best_param_multivalent[ "e" ] = optimal_parameters[ 4 ]
+  aic_multi = AIC( log_likelyhood_multi, optimal_parameters_multi )
   
-  print( f"Try fitting model where NO signal is assumed (constant)" )
+  print( "Try fitting model where NO signal is assumed (constant)" )
   #Next, assume a model where there is no signal
-  optimal_parameters, onset_parameters = find_best_fit_mle( x_data, y_data, 
+  par, _, best_sample, _ = find_best_fit(  
+                                                     x_data = x_data_all, 
+                                                     y_data = y_data_all, 
+                                                     bounds = bounds, 
+                                                     initial_guess = initial_guess, 
+                                                     function_type = "constant",
+                                                     onset_fitting = False,
+                                                     mc_runs = 1, 
+                                                     n_hopping = 1000, 
+                                                     #n_hopping = n_hopping, 
+                                                     #T_hopping = T_hopping 
+                                                     )
+  
+  initial_guess = par[ best_sample, :1 ]
+  print( f"Initial guess for gp optimisation {initial_guess}" )
+  
+  optimal_parameters_const, log_likelyhood_const, _ = find_best_fit_mle( 
+                   no_signal, 
+                   x_data_all, y_data_all, 
                    bounds, initial_guess, function_type = "constant",
-                   kernel = RBF(),
-                   alpha_gp =1e-10, # Small alpha for likelihood
-                   n_restarts_optimizer_gp = 10 
+                   kernel = kernel,
+                   alpha_gp = alpha_gp, # Small alpha for likelihood
+                   n_restarts_optimizer_gp = n_restarts_optimizer_gp, 
                    onset_fitting = False,
                    verbose = False )
-
-  best_param_constant = {} 
-  best_param_constant[ "a" ] = optimal_parameters[ 0 ]
+  
+  aic_const = AIC( log_likelyhood_const, optimal_parameters_const )
 
   # Calculate Akaike information criterion to compare the two models
+  delta_aic = aic_multi - aic_const
 
   signal_present = False 
   if delta_aic > 0:
     signal_present = True
+    print( "Signal encountered" )
 
-  # This is a bit redundant but I put it for clarity
-  signal_present = test_passed
-
-  print( f"Signal encountered" )
   #Ok, now also save this data
   with open( stat_file, 'w+' ) as my_f:
     my_f.write( f"Complex model is a better representation? {signal_present} \n" )
@@ -142,17 +169,17 @@ for name in sheet_names:
     with open( output_file, "w+" ) as my_f:
       my_f.write( "Optimal parameters for onset: \n" )
       for i, value in onset_parameters.items():
-      my_f.write( f"{i}  {value}" )
+        my_f.write( f"{i}  {value}" )
 
-    plot_fitted_curve_mle( x_data_all, y_data_all, 
-                 optimal_parameters, 
+    plot_fitted_curve( x_data_all, y_data_all, 
+                 optimal_parameters_multi, 
                  onset_coeffs = onset_parameters, 
                  function_type = function_type, 
                  graph_name = output_graph, 
                  verbose = False,
                  same_scale = False,
                  mle = True )
-    onset_str = f"{best_onset_coeffs[ 'onset' ]}"
+    onset_str = f"{onset_parameters[ 'onset' ]}"
 
   else:
     function_type = "constant"
@@ -162,7 +189,7 @@ for name in sheet_names:
     # Plot the graph. Add Tag SIGNAL_NOT_DETECTED if the signal is undetectable = model assuming no binding better 
     # fits the data
     plot_fitted_curve( x_data_all, y_data_all, 
-                   optimal_parameters, 
+                   optimal_parameters_const, 
                    onset_coeffs = onset_parameters, 
                    function_type = function_type, 
                    graph_name = output_graph + tag, 
